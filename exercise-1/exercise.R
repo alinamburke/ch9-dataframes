55494d60b6d682a7c86a3e2933756f186e6381b2
Alina Burke <alinamburke@gmail.com>
2017-10-12 14:46:48 -0700

    Complete exercise 1, start exercise 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,20 +2,24 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games of their season
 # Hint: (google "Seahawks scores")
+seahawks.points <- c(48, 20, 26, 17)
 
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+opponents.points <- c(17, 13, 13, 13)
 
 # Combine your two vectors into a dataframe
-
+games <- data.frame(seahawks.points, opponents.points)
 
 # Create a new column "diff" that is the difference in points
+diff <- c(seahawks.points-opponents.points)
+games <- data.frame(games, diff)
 
-
-# Create a new column "won" which is TRUE if the Seahawks wom
-
+# Create a new column "won" which is TRUE if the Seahawks won
+won <- c(diff > 0)
+games <- data.frame(games, won)
 
 # Create a vector of the opponents
-
+opponents <- c("Chargers", "Vikings", "Chiefs", "Raiders")
 
 # Assign your dataframe rownames of their opponents
+rownames(games) <- opponents
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,22 +1,26 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- c(1:100)
+employees <- (paste0("Employee ", employees[]))
 
 # Create a vector of 2014 salaries using the runif function
-
+fourteen.salaries <- c(runif(100, min = 2, max = 100000))
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+fifteen.salaries <- c(runif(100, min = 5, max = 150000))
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(fourteen.salaries, fifteen.salaries)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+raise <- c(fifteen.salaries-fourteen.salaries)
+salaries <- data.frame(salaries, raise)
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+got.raise <- c(raise > 0)
+salaries <- data.frame(salaries, got.raise)
 
 # Retrieve values from your data frame to answer the following questions:
+row.names(salaries) <- employees
 
 # What was the 2015 salary of employee 57
 
